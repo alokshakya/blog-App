f6514e191ec76c32d3095bec6126093b1e65ce1c
alokshakya <alokshakya75@gmail.com>
2017-08-03 02:05:52 +0530

    all links working except trending popular

---
+++ app/src/js/controllers/SportsController.js
@@ -0,0 +1,83 @@
+CApp.controller("SportsController",
+ function ($scope, $http, $templateCache,$rootScope,AuthService,Data,DataService) {
+     $scope.all={};
+     $scope.all.articles=[];
+    $scope.all.data={};
+    // alert(' current date '+$scope.all.date);
+     $scope.all.busy=false;
+     $scope.all.end=false;
+     $scope.all.error='';
+     $scope.all.error_condition=false;
+
+       $scope.all.user_id=AuthService.getUserId();
+    if(!$scope.all.user_id)
+        {
+          alert('Login First to visit this page');
+          window.location.href='/#/login';
+          
+        }
+     //function for loading new articles
+     $scope.all.nextPage = function()
+     {
+      if((!$scope.all.busy) && ($scope.all.end))
+      {
+        return;
+      }
+      $scope.all.busy=true;
+    var cr;
+    if(!$scope.all.articles[$scope.all.articles.length-1])
+    {
+      cr=new Date();
+    }
+    if($scope.all.articles[$scope.all.articles.length-1])
+    {
+      cr=$scope.all.articles[$scope.all.articles.length-1].created;
+    }
+var query=
+{
+  "type":"select",
+  "args":{
+    "table":"article",
+    "columns":["article_id","title","category","created","content",
+    {
+      "name":"published_by",
+      "columns":["name"]
+    }
+    ],
+    "where":
+    {
+      "$and":[{"created":{"$lt": cr}},{"category":{"$eq":"Sports"}}]
+    },
+    
+    "order_by":"-created",
+    "limit":5
+  }
+};
+
+              DataService.addLike(JSON.stringify(query))
+              .then(function successCallback(response) {
+                var l=response.data.length;
+                      if(l===0)
+                      {
+                        $scope.all.busy=false;
+                        $scope.all.end=true;
+                        return;
+                      }
+                $scope.all.article_id=response.data[l-1].article_id;
+                console.log('response.data[l-1].article_id '+response.data[l-1].article_id);
+                console.log('new article_id '+$scope.all.article_id);
+                      for(var i=0;i<l;i++)
+                      {
+                        $scope.all.articles.push(response.data[i]);
+                      }
+                $scope.all.busy=false;
+                }, function errorCallback(response) {
+                   $scope.all.error='Request failed at server please check Internet_Connection';
+                   $scope.all.error_condition=true;
+
+                });
+
+
+     };
+     
+});
